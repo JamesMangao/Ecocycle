bce73c231cf37b7a9fef33f4125a6393b23d4431
James Mangao <144905900+JamesMangao@users.noreply.github.com>
2026-02-06 16:49:17 +0800

    Refactor BackButton import to named import

--- app/bins/[binId]/page.tsx
+++ app/bins/[binId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react';
 import withAuth from '../../components/withAuth';
-import BackButton from '../../components/BackButton';
+import { BackButton } from '../../components/BackButton';
 import { usePathname } from 'next/navigation';
 
 interface Detection {
